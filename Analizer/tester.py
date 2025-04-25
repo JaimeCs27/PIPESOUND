de87c71b07c39831b89b61a4144c6323ca79df62
JaimeCs27 <jaimecab2002@gmail.com>
2025-04-24 22:23:10 -0600

    Arbimon Module class
    Implementacion de la clase arbimon module

--- Analizer/tester.py
+++ Analizer/tester.py
@@ -1,30 +1,28 @@
-from Analizer import Analizer
-from acoustic_index import *
 from scipy import signal
 from os import walk, path
 from datetime import datetime
 import os
-from progress import load_last_processed_file, save_last_processed_file, reset_progress, PROGRESS_FILE, analize
-
+from ArbimonModule import ArbimonModule
 
 def test():
-    analizer = Analizer('config/config.yaml')
-    csv_path = "prueba.csv"
-    indices = ['Acoustic_Complexity_Index', 'Bio_acoustic_Index', 'Acoustic_Diversity_Index',
-               'Acoustic_Evenness_Index', 'Normalized_Difference_Sound_Index', 'Spectral_Entropy',
-               'NB_peaks', 'Temporal_Entropy', 'Wave_Signal_To_Noise_Ratio']
-    analizer.set_headers(indices, csv_path)
+    arbModule = ArbimonModule()
+    project = None
+    try:
+        project = arbModule.Load_projects()[0]
+    except:
+        print("Error loading user projects.")
+    sites = None
+    try:
+        sites = arbModule.Load_Sites(project['id'])
+        for site in sites:
+            print(site['name'])
+    except:
+        print("Error while loading " +project['name'] + " sites")
 
-    last_file = load_last_processed_file()
-
-    if last_file:
-        choice = input(f"El programa fue interrumpido repentinamente, se encontró progreso previo en '{last_file}'. ¿Desea continuar desde allí? (s/n): ")
-        if choice.lower() != 's':
-            reset_progress()
-            last_file = None
-
-    analize('Test_audios', analizer, indices, csv_path, last_file)
-    
+    try:
+        arbModule.Download_Project(sites, "D:/Universidad/Semestre VII/Proyecto/PIPESOUND/Prueba", "Proyecto 1")
+    except:
+        print("Error while Downloading the files")
 
 
 
